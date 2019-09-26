ce5b864f11b2df94f6deb1d6fe1875df3bdeec54
JullyaneAlves <jullyanet94@gmail.com>
2019-09-26 18:47:11 -0300

    Atualização

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
 from .models import Agenda
+from .models import Livraria
+from .models import  Despesa
+from .models import Compras
+from .models import Apartamento
+from .models import Anuncio
+
 
 from .models import *
 
@@ -16,12 +22,45 @@ admin.site.register(Autor)
 admin.site.register(Aluno, AlunoAdmin)
 admin.site.register(Emprestimo, EmprestimoAdmin)
 
-
-
 class AgendaAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'descricao')
 
 
 admin.site.register(Agenda, AgendaAdmin)
+
+class LivrariaAdmin(admin.ModelAdmin):
+    list_display = ('Titulo', 'Nome_do_autor', 'Assunto', 'Editora', 'ISBN')
+
+admin.site.register(Livraria, LivrariaAdmin)
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'telefone')
+
+admin.site.register(Contato, ContatoAdmin)
+
+
+class  DespesaAdmin(admin.ModelAdmin):
+    list_display = ('data_criacao' , 'tipo_despesa', 'descricao', 'forma_pagamento' )
+
+admin.site.register(Despesa,  DespesaAdmin)
+
+
+class ComprasAdmin(admin.ModelAdmin):
+    list_display = ('nome' , 'descricaoProduto')
+
+admin.site.register(Compras, ComprasAdmin)
+
+
+class ApartamentoAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'proprietario')
+
+admin.site.register(Apartamento, ApartamentoAdmin)
+
+class AnuncioAdmin(admin.ModelAdmin):
+    list_display = ('cliente' , 'textoTitulo', 'textoAnuncio', 'nomeContato', 'telefone', 'secao', 'tipoAnuncio')
+
+admin.site.register(Anuncio, AnuncioAdmin)
+
 
 
